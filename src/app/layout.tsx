0ba50ae75a641b73d431643bb63e5e560d8c156c
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2025-03-28 22:04:28 +0100

    Added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,20 @@ import "@/styles/globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Reflex IP",
-  description: "A GEOIP service comparison utility."
+  title: "Nantric | Compare IP Databases with Ease",
+  description: "Quickly compare IP lookup results across top IP databases. Discover accuracy, consistency, and insights with Nantric's streamlined IP comparison tool.",
+  openGraph: {
+    type: "website",
+    url: "http://reflex.nantric.net/",
+    title: "Nantric | Compare IP Databases with Ease",
+    description: "Quickly compare IP lookup results across top IP databases. Discover accuracy, consistency, and insights with Nantric's streamlined IP comparison tool.",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/ThijmenGThN/ReflexIP/refs/heads/main/src/assets/preview.png",
+        alt: "Nantric Meta Image",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
